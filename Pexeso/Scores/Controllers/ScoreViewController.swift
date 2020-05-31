54f7caaf3b9174fea7ea078c0d4c0d9ac70e380d
version-1 <j.onodera26@gmail.com>
2020-05-31 11:47:56 -0700

    style table

--- Pexeso/Scores/Controllers/ScoreViewController.swift
+++ Pexeso/Scores/Controllers/ScoreViewController.swift
@@ -8,10 +8,15 @@
 import UIKit
 
 class ScoreViewController: UITableViewController {
-
+    
+    struct PropertyKeys {
+        static let cell = "cell"
+    }
+    
+    let list = ScoreList.instance.scores
+    let DISPLAY_ROW_COUNT = 5
     override func viewDidLoad() {
         super.viewDidLoad()
-//        self.navigationItem.title = "Scores"
         UIGraphicsBeginImageContext(self.view.frame.size)
         UIImage(named: "Table_03")?.draw(in: self.view.bounds)
         
@@ -21,16 +26,55 @@ class ScoreViewController: UITableViewController {
         } else {
             UIGraphicsEndImageContext()
             debugPrint("Image not available")
-         }
+        }
         
-//        let backBarButton = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancelScoreView(_:)))
-//        self.navigationItem.leftBarButtonItem = backBarButton
+        setBody()
     }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.navigationController?.setNavigationBarHidden(true, animated: animated)
     }
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if list.count < DISPLAY_ROW_COUNT {
+            return list.count
+        }
+        return DISPLAY_ROW_COUNT
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        let header = view as! UITableViewHeaderFooterView
+        header.backgroundView = UIView(frame: self.view.bounds)
+        header.backgroundView?.backgroundColor = UIColor.clear
+        header.textLabel?.textColor = UIColor.clear
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection
+        section: Int) -> String? {
+        return "Header"
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: PropertyKeys.cell, for: indexPath)
+        
+        let row = self.list[indexPath.row]
+        
+        cell.textLabel?.text =  String(indexPath.row + 1) + ".  " + row.name + " " + String(row.score)
+        cell.textLabel?.textAlignment = .center
+        cell.textLabel?.font = UIFont(name: "TimesNewRomanPS-ItalicMT", size: 20.0)!
+        cell.backgroundColor = UIColor(named: "transparent")
+        
+        return cell
+    }
+    
+    func setBody() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: PropertyKeys.cell)
+        tableView.sectionHeaderHeight = CGFloat(150.0)
+        tableView.separatorColor = UIColor.clear
+        tableView.allowsSelection = false
+        tableView.tableFooterView = UIView(frame: CGRect.zero)
+    }
     
     @objc func cancelScoreView(_ sender: UIBarButtonItem) {
         dismiss(animated: true, completion: nil)
